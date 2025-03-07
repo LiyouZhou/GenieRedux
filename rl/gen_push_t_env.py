fafdee4a25697b6e0a47f5b6cb2639ea037a10e8
Liyou Zhou <liyou.zhou@outlook.com>
2025-03-07 16:31:15 +0000

    Generate next frame based on last 15 frames

--- rl/gen_push_t_env.py
+++ rl/gen_push_t_env.py
@@ -14,9 +14,9 @@ from models.genie_redux import GenieReduxGuided
 
 
 class GenerativePushT(Env):
-    def __init__(self, model: GenieReduxGuided, goal_model, initial_state_path):
+    def __init__(self, model: GenieReduxGuided, goal_model, ds):
         super().__init__()
-        self.action_space = spaces.Box(-1, 1, shape=(15, 2))
+        self.action_space = spaces.Box(-1, 1, shape=(2,))
         self.observation_space = spaces.Box(low=-np.inf, high=np.inf, shape=(256, 32))
         self.model = model
         self.model = self.model.to("cuda")
@@ -29,30 +29,49 @@ class GenerativePushT(Env):
         self.goal_model = self.goal_model.to("cuda")
         self.goal_model.eval()
 
-        self.initial_states = np.load(initial_state_path)
-        self.initial_poses = np.load(initial_state_path.replace("latents", "poses"))
+        self.ds = ds
+        episode_ends = list(ds.replay_buffer.meta.episode_ends)
+        self.episode_starts = [0] + episode_ends[:-1]
 
         self.reset()
+
+    def observe(self):
+        self.state[-1]
+        return
 
     def reset(self, seed=None):
         # select random index for initial state and pose
         random.seed(seed)
-        idx = randint(0, len(self.initial_states))
-        self.initial_state = self.initial_states[idx]
-        self.initial_pose = self.initial_poses[idx]
-        self.state = torch.tensor(self.initial_state).to("cuda")
-        self.pose = self.initial_pose
+        num_episodes = len(self.episode_starts)
+        idx = randint(0, num_episodes - 1)
+        idx = 0
+        start_idx = self.episode_starts[idx]
+        episode_actions = self.ds.replay_buffer.data.action[start_idx : start_idx + 15]
+        episode_obs = self.ds.replay_buffer.data.camera_3[start_idx : start_idx + 15]
+
+        episode_obs = torch.tensor(episode_obs).to("cuda")
+        episode_obs = episode_obs / 255.0
+        episode_obs = rearrange(episode_obs, "(b f) h w c -> b c f h w", b=1)
+
+        with torch.no_grad():
+            frame_latent = self.model.tokenizer(episode_obs, return_only_codebook_ids=True)
+
+        self.state = frame_latent
+        self.actions = torch.tensor(episode_actions).to("cuda")
+        self.actions = rearrange(self.actions, "(b f) ... -> b f ...", b=1)
+
+        print(f"state: {self.state.shape}")
+        print(f"actions: {self.actions.shape}")
 
         self.step_count = 0
-        info = {"step": self.step_count, "scores": None}
+        info = {"step": self.step_count, "scores": None, "frames": episode_obs.clone()}
 
-        return self.obs, info
+        return self.state, info
 
     def reached_goal(self):
         with torch.no_grad():
-            latent_codes = self.model.tokenizer.vq.codebook[self.state]
-            latent_codes = latent_codes.unsqueeze(0)
-            latent_codes = rearrange(latent_codes, "b ... -> b (...)")
+            latent_codes = self.model.tokenizer.vq.codebook[self.state[0, -1]]
+            latent_codes = rearrange(latent_codes, "... -> 1 (...)")
             reward = self.goal_model(latent_codes)
             reward = nn.Softmax(dim=1)(reward)
             pred = reward.cpu().numpy()
@@ -60,33 +79,25 @@ class GenerativePushT(Env):
 
         return reward == 1
 
-    def step(self, actions):
-        poses = []
-        initial_pose = self.pose
-        for action in actions:
-            pose = np.add(initial_pose, action)
-            poses.append(pose)
-            initial_pose = pose
+    def step(self, action):
+        action = torch.tensor(action).to("cuda")
+        action = rearrange(action, "(b f d) -> b f d", b=1, f=1)
+        action += self.actions[:, -1]
+        self.actions = torch.cat([self.actions[:, 1:], action], dim=1)
+        print("self.actions", self.actions.shape, self.actions.dtype)
+        print("self.state", self.state.shape, self.actions.dtype)
 
         with torch.no_grad():
-            with torch.autocast(device_type="cuda"):
-                poses = np.stack(poses)
-                poses = torch.tensor(poses).float().to("cuda")
-                poses = poses.unsqueeze(0)
+            output, scores = self.model.dynamics.sample(
+                prime_token_ids=rearrange(self.state, "b ... -> b (...)"),
+                num_tokens=self.model.num_tokens_per_frames(15, 1),
+                actions=self.actions.float(),
+                patch_shape=self.model.get_video_patch_shape(16, 1),
+                return_confidence=True,
+            )
 
-                prime_token_ids = torch.unsqueeze(self.state, 0)
-                prime_token_ids = rearrange(prime_token_ids, "b ... -> b (...)")
-
-                output, scores = self.model.dynamics.sample(
-                    prime_token_ids=prime_token_ids,
-                    num_tokens=self.model.num_tokens_per_frames(15, 1),
-                    actions=poses,
-                    patch_shape=self.model.get_video_patch_shape(16, 1),
-                    return_confidence=True,
-                )
-
-        output = output.reshape(15, 256)
-        self.state = output[0].squeeze()
+        output = rearrange(output, "b (f w h) -> b f w h", b=1, w=16, h=16)
+        self.state = torch.cat([self.state[:, 1:], output[:, -1:]], dim=1)
         info = {"step": self.step_count, "scores": scores}
         score = torch.mean(info["scores"])
 
@@ -116,62 +127,45 @@ class GenerativePushT(Env):
         return obs.cpu().numpy()
 
     def render(self):
-        indices = self.state
-        indices = indices.reshape(1, 1, 16, 16)
-        dummy_first_frame = indices.clone()
-        indices = torch.cat([dummy_first_frame, indices], dim=1)
-        image = self.model.decode_from_codebook_indices(indices)
-        image = image.squeeze()
-        image = image[:, 1]
-        image = rearrange(image, "c h w -> h w c")
-
+        image = self.model.decode_from_codebook_indices(self.state)
+        image = rearrange(image, "b c f h w -> (b f) h w c")
+        image = image[-1]
         return image.cpu().numpy()
 
 
 if __name__ == "__main__":
     from pathlib import Path
-
-    from hydra import compose, initialize
     from matplotlib import pyplot as plt
-    from omegaconf import DictConfig
     from reward_from_latents import RewardFromLatents
-
-    from models import construct_model
+    from model_utils import construct_model_from_checkpoint, create_push_t_image_dataset
 
     base_dir = Path(__file__).resolve().parent.parent
-
-    print(base_dir)
-
-    with initialize(version_base=None, config_path=f"../configs", job_name="test_app"):
-        cfg = compose(config_name="config/genie_redux_guided.yaml")
-
-    cfg = DictConfig(cfg.config)
-    cfg.tokenizer_fpath = (
-        f"{base_dir}/checkpoints/tokenizer/tokenizer_push_t_fp32/model-50000.pt"
+    model = construct_model_from_checkpoint(
+        "checkpoints/tokenizer/tokenizer_250213112859/model-50000.pt",
+        "checkpoints/genie_redux_guided/genie_redux_guided_250217235935/model-100000.pt",
     )
-
-    pt_dataset_path = f"{base_dir}/datasets/pusht_real/real_pusht_20230105"
-    image_size = (64, 64)
-    num_frames = 16
-
-    model = construct_model(
-        config=cfg,
+    torch.serialization.add_safe_globals([RewardFromLatents])
+    goal_model = torch.load("pusht_goal_reward_from_latents.pth", weights_only=False)
+    ds = create_push_t_image_dataset(
+        pt_dataset_path="datasets/pusht_real/pusht_real",
     )
-
-    model.load_state_dict(
-        torch.load(
-            f"{base_dir}/checkpoints/genie_redux_guided/genie_redux_guided_push_t_fp32/model-100000.pt"
-        )["model"]
-    )
-
-    goal_model = torch.load("pusht_goal_reward_from_latents.pth")
     env = GenerativePushT(
         model,
         goal_model,
-        initial_state_path=f"{base_dir}/datasets/pusht_goal/pusht_goal/videos/initial_frame_latents.npy",
+        ds,
     )
 
-    state = env.reset()
+    state, info = env.reset()
+
+    seed_imgs = info["frames"]
+    print("seed_imgs", seed_imgs.shape)
+    # seed_imgs = rearrange(seed_imgs, "b c f h w -> (b f) h w c")
+    for i in range(seed_imgs.shape[0]):
+        img = ds.replay_buffer.data.camera_3[0 : i + 1]
+        print("img", img.shape)
+
+        plt.imsave(f"seed_img_{i}.png", seed_imgs[i].cpu())
+
 
     num_frames = 10
     fig, axs = plt.subplots(1, num_frames, figsize=(20, 5))
@@ -179,4 +173,12 @@ if __name__ == "__main__":
         img = env.render()
         img = img.clip(0, 1)
         axs[i].imshow(img)
-        env.step(np.full([15, 2], 0.01))
+        plt.imsave(f"img_{i}.png", img)
+        env.step(
+            np.full(
+                [
+                    2,
+                ],
+                0.01,
+            )
+        )
